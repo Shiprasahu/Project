78ab3127686406c6ebd5f583d66c025417f16d1f
Shiprasahu <siprasahu18@gmail.com>
2019-08-27 13:26:31 +0530

    commit

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'flaskapp.db')
